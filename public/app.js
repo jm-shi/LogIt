84e70b86a475fbbe460cbf7ab3fc85759463bc69
Jamie S <jamieshi2u@gmail.com>
2018-06-23 01:40:39 -0700

    Set up Cloud Firestore

---
+++ public/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyD_BUKEpF5nKmVSh_Dxw1Wpa_UOdBZh-oQ",
+    authDomain: "logitcalendar.firebaseapp.com",
+    databaseURL: "https://logitcalendar.firebaseio.com",
+    projectId: "logitcalendar",
+    storageBucket: "logitcalendar.appspot.com",
+    messagingSenderId: "843432594890"
+};
+firebase.initializeApp(config);
