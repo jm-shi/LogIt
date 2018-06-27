e17612891ce7e500b69aac4fe115e28ba8094093
Jamie S <jamieshi2u@gmail.com>
2018-06-26 19:04:49 -0700

    Cleanup

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,14 @@
 window.onload = function() {
     document.getElementById("signout").addEventListener("click", logout);
-    document.getElementById("createbtn").addEventListener("click", createTask);
+    document.getElementById("createbtn").addEventListener("click", displayCreateScreen);
     document.getElementById("hamburger").addEventListener("click", toggleSidebar);
+
     document.getElementsByClassName("close")[0].addEventListener("click", close);
+    document.getElementsByClassName("close")[1].addEventListener("click", close);
     window.addEventListener("click", closeOutside);
 
-    (document.getElementById("todo") !== null) ? document.getElementById("todo").addEventListener("click", viewTodo) : "";
-    (document.getElementById("completed") !== null) ? document.getElementById("completed").addEventListener("click", viewCompleted) : "";
+    (document.getElementById("todo") !== undefined) ? document.getElementById("todo").addEventListener("click", viewTodo) : "";
+    (document.getElementById("completed") !== undefined) ? document.getElementById("completed").addEventListener("click", viewCompleted) : "";
 }
 
 function logout() {
@@ -17,21 +19,19 @@ function logout() {
     });
 }
 
-function createTask() {
-    window.location.href = "createTask.html";
-}
-
 function toggleSidebar() {
     document.getElementById("sidebar").classList.toggle("active");
 }
 
 function close() {
-    document.getElementById("modal").style.display = "none";
+    document.getElementsByClassName("modal")[0].style.display = "none";
+    document.getElementsByClassName("modal")[1].style.display = "none";
 }
 
 function closeOutside(e) {
-    if (e.target === modal) {
-        document.getElementById("modal").style.display = "none";
+    if (e.target.className === "modal") {
+        document.getElementsByClassName("modal")[0].style.display = "none";
+        document.getElementsByClassName("modal")[1].style.display = "none";
     }
 }
 
@@ -43,22 +43,14 @@ function viewCompleted() {
     window.location.href = "completed.html";
 }
 
-function revise(taskID, type) {
+function revise(taskID, type, createNewTask) {
     var title = document.getElementById("title").value;
     var date = document.getElementById("date").value;
     var time = document.getElementById("time").value;
     var description = document.getElementById("description").value;
-    
-    if (title === "") {
-        alert("Please enter a title.");
-        return;
-    }
-    if (date === "") {
-        alert("Please enter a date.");
-        return;
-    }
-    if (time === "") {
-        alert("Please enter a time.");
+
+    if (title === "" || date === "" || time === "") {
+        alert("You must enter a title, date, and time.");
         return;
     }
 
@@ -66,25 +58,84 @@ function revise(taskID, type) {
     if (timestamp === undefined || timestamp === "") timestamp = 0;
     var user = firebase.auth().currentUser;
 
-    database.collection("users").doc(user.uid).collection(type).doc(taskID).set({
-        title: title,
-        date: date,
-        time: time,
-        description: description,
-        timestamp: timestamp
-    }).then(function() {
-        document.getElementsByClassName("modal-list")[0].innerHTML = "";
-        location.reload();
-    }).catch(function(error) {
-        console.log("Error: " + error);
+    if (!createNewTask) {
+        database.collection("users").doc(user.uid).collection(type).doc(taskID).set({
+            title: title,
+            date: date,
+            time: time,
+            description: description,
+            timestamp: timestamp
+        }).then(function() {
+            document.getElementsByClassName("modal-list")[0].innerHTML = "";
+            location.reload();
+        }).catch(function(error) {
+            console.log("Error: " + error);
+        });
+    }
+    else {
+        database.collection("users").doc(user.uid).collection("todo").doc().set({
+            title: title,
+            date: date,
+            time: time,
+            description: description,
+            timestamp: timestamp
+        }).then(function() {
+            document.getElementsByClassName("modal-list")[1].innerHTML = "";
+            location.reload();
+        }).catch(function(error) {
+            console.log("Error: " + error);
+        });
+    }
+}
+
+function displayCreateScreen() {
+    document.getElementsByClassName("modal")[1].style.display = "block";
+
+    var info = `<label for=title>Title</label>
+    <input type=text id=title name=title placeholder='Enter a title'><br>
+    <label for=date>Date</label>
+    <input type=date id=date name=date placeholder='Enter a date'><br>
+    <label for=time>Time</label>
+    <input type=time id=time name=time placeholder='Enter a time'><br>
+    <label for=description>Description</label>
+    <textarea id=description name=description placeholder='Enter a description'></textarea><br>
+    <button id='create' onclick="revise( '', 'todo', true )">Create</button>`;
+
+    document.getElementsByClassName("modal-list")[1].innerHTML = info;
+}
+
+function completeTask(clickedID) {
+    var taskID = clickedID.slice(5);
+    var userID = firebase.auth().currentUser.uid;
+    var date, description, timestamp, title;
+    database.collection("users").doc(userID).collection("todo").doc(taskID).get().then(function(doc) {
+        if (doc.exists) {
+            date = doc.data().date;
+            description = doc.data().description;
+            timestamp = doc.data().timestamp;
+            title = doc.data().title;
+            database.collection("users").doc(userID).collection("completed").doc(taskID).set({
+                date: date,
+                description: description,
+                timestamp: timestamp,
+                title: title
+            }).then(function() {
+                database.collection("users").doc(userID).collection("todo").doc(taskID).delete().then(function() {
+                    console.log("Successfully deleted document");
+                    document.getElementById(taskID).style.display = "none";
+                }).catch(function(error) {
+                    console.log("Error removing document: ", error);
+                });
+            });
+        }
     });
 }
 
-function editTask(clickedID, type) {
+function displayEditScreen(clickedID, type) {
     var taskID = clickedID.slice(5);
     var userID = firebase.auth().currentUser.uid;
     var date, time, description, timestamp, title;
-    document.getElementById("modal").style.display = "block";
+    document.getElementsByClassName("modal")[0].style.display = "block";
     database.collection("users").doc(userID).collection(type).doc(taskID).get().then(function(doc) {
         if (doc.exists) {
             date = doc.data().date;
@@ -101,7 +152,7 @@ function editTask(clickedID, type) {
                         <input type=time id=time name=time placeholder='Enter a time' value=${time}><br>
                         <label for=description>Description</label>
                         <textarea id=description name=description placeholder='Enter a description' value=${description}></textarea><br>
-                        <button id=revise onclick="revise( '${taskID}', '${type}' )">Revise</button>`;
+                        <button id=revise onclick="revise( '${taskID}', '${type}', false )">Revise</button>`;
 
             document.getElementsByClassName("modal-list")[0].innerHTML = info;
         }
