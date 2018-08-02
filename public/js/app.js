3faa30b7654e0a3cc8e9a9bf717e8827cc6babfb
Jamie S <jamieshi2u@gmail.com>
2018-08-01 23:36:33 -0700

    Use ES6

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,20 @@
-var config = {
-    apiKey: "AIzaSyD_BUKEpF5nKmVSh_Dxw1Wpa_UOdBZh-oQ",
-    authDomain: "logitcalendar.firebaseapp.com",
-    databaseURL: "https://logitcalendar.firebaseio.com",
-    projectId: "logitcalendar",
-    storageBucket: "logitcalendar.appspot.com",
-    messagingSenderId: "843432594890"
+const config = {
+  apiKey: 'AIzaSyD_BUKEpF5nKmVSh_Dxw1Wpa_UOdBZh-oQ',
+  authDomain: 'logitcalendar.firebaseapp.com',
+  databaseURL: 'https://logitcalendar.firebaseio.com',
+  projectId: 'logitcalendar',
+  storageBucket: 'logitcalendar.appspot.com',
+  messagingSenderId: '843432594890'
 };
 firebase.initializeApp(config);
 
 firebase.firestore().settings({
-    timestampsInSnapshots: true
+  timestampsInSnapshots: true
 });
 
-firebase.firestore().enablePersistence()
-    .catch(function(err) {
-        console.log(err);
-    }); 
+firebase
+  .firestore()
+  .enablePersistence()
+  .catch(function(err) {
+    console.log(err);
+  });
